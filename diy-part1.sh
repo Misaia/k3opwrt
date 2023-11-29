e19f4236992bda2e8e0e1cc5d6412e6e6a5f1385
Misaia <3131667831@qq.com>
2023-11-29 13:38:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,4 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git helloworld2 https://github.com/Misaia/helloworld.git' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/kenzok8/small-package.git' >>feeds.conf.default
